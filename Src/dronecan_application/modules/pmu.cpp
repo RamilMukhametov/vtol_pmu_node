fa4f69093b0bb10b617db084dd6671864225dc58
PonomarevDA <ponomarevda96@gmail.com>
2024-01-12 10:56:33 +0300

    fix adc multipliers

--- Src/dronecan_application/modules/pmu.cpp
+++ Src/dronecan_application/modules/pmu.cpp
@@ -12,6 +12,8 @@
 #include "periphery/adc/adc.hpp"
 #include "uavcan/protocol/debug/LogMessage.h"
 
+#define ADC_VOLTAGE_MULTIPLIER (19.0 * 3.3 / 4096.0) // 62.7  Volts when ADC is 3.3V (4095)
+#define ADC_CURRENT_MULTIPLIER 0.04884004884         // 200.0 Amper when ADC is 3.3V (4095)
 
 VtolPmu::VtolPmu() {
 
@@ -29,13 +31,15 @@ int8_t VtolPmu::init() {
     _battery_info.state_of_charge_pct_stdev = 0;
 
     DebugLogMessage_t msg{};
-    msg.source_size = 2;
-    msg.source[0] = 'h';
-    msg.source[1] = 'i';
-    msg.text[0] = '4';
-    msg.text[1] = '2';
-    msg.text[2] = '.';
-    msg.text_size = 3;
+    msg.source_size = 3;
+    msg.source[0] = 'p';
+    msg.source[1] = 'm';
+    msg.source[1] = 'u';
+    msg.text[0] = 'i';
+    msg.text[1] = 'n';
+    msg.text[2] = 'i';
+    msg.text[3] = 't';
+    msg.text_size = 2;
     uint8_t transfer_id = 0;
     dronecan_protocol_debug_log_message_publish(&msg, &transfer_id);
 
@@ -54,7 +58,7 @@ void VtolPmu::process() {
 void VtolPmu::_spin_once() {
     _last_spin_time_ms = HAL_GetTick();
 
-    float voltage = AdcPeriphery::get(AdcChannel::ADC_VIN) * 19.0 * 3.3 / 4096.0;
+    float voltage = AdcPeriphery::get(AdcChannel::ADC_VIN) * ADC_VOLTAGE_MULTIPLIER;
     _battery_info.voltage = voltage;
 
     uint16_t adc_temperature = AdcPeriphery::get(AdcChannel::ADC_TEMPERATURE);
@@ -64,7 +68,7 @@ void VtolPmu::_spin_once() {
     float kelvin = (ADC_REF - adc_temperature) / AVG_SLOPE + TEMP_REF + 273.15;
     _battery_info.temperature = kelvin;
 
-    float current = AdcPeriphery::get(AdcChannel::ADC_CRNT) * 600.0 * (3.3 / 3.0 / 4096.0);
+    float current = AdcPeriphery::get(AdcChannel::ADC_CRNT) * ADC_CURRENT_MULTIPLIER;
     _battery_info.current = current;
 
     dronecan_equipment_battery_info_publish(&_battery_info, &_transfer_id);
